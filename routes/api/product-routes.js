f9bd4fc49d4b2842dde7f336bb3ad0cd03c7b2b7
Leon Hsu <leonhsu95@gmail.com>
2021-05-30 21:47:07 +1000

    Product routes query established

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category, attributes: ['category_name'] },{ model: Tag, attributes: ['tag_name']}],
+      include: [{ model: Category },{ model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -21,6 +21,14 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 // create new product
@@ -33,6 +41,7 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ 
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
